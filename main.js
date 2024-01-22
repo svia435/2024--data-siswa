a8e4c7897a8c1f3f150d34c845048a4a370d19a9
svia435 <svia435@gmail.com>
2024-01-22 11:13:41 +0700

    Menambah konfigurasi firebase

---
+++ main.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { 
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRlnLBhUHGOF7Lfg9iy_SbfK6coM_7f1U",
+  authDomain: "insan-cemerlang-6640c.firebaseapp.com",
+  projectId: "insan-cemerlang-6640c",
+  storageBucket: "insan-cemerlang-6640c.appspot.com",
+  messagingSenderId: "917464283158",
+  appId: "1:917464283158:web:3a6179cd71818d68f6dd37"
+};
+
+const app = initializeApp(firebaseConfig);
